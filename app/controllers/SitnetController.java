752f66546a67e8f72a3b9d4089d6fdf18e0966b1
Miikka Liukkonen <miikka.liukkonen@arcusys.fi>
2014-02-10 09:35:34 +0200

    remove extra methods, change bindForm exception message to include actual problems

--- app/controllers/SitnetController.java
+++ app/controllers/SitnetController.java
@@ -12,30 +12,15 @@ import play.mvc.Result;
 
 public class SitnetController extends Controller {
 
-
     public static final String SITNET_TOKEN_HEADER_KEY = "x-sitnet-authentication";
     public static final String SITNET_CACHE_KEY = "user.session.";
     public static final int SITNET_TIMEOUT_MINUTES = 30;
 
-
-    static boolean isTokenValid() {
-        Period tokenValidPeriod = new Period().withMinutes(30);
-        String token = request().getHeader("SITNET_TOKEN_HEADER_KEY");
-        Session session = (Session) Cache.get(SITNET_CACHE_KEY + token);
-        return session != null ? true : false;
-    }
-
     static <T> T bindForm(Class<T> clazz) throws MalformedDataException {
         Form<T> form = Form.form(clazz);
         if (form.hasErrors()) {
-            throw new MalformedDataException("Form data didn't apply with the constrains of class: " + clazz.getName());
+            throw new MalformedDataException(form.errorsAsJson().asText());
         }
         return form.bindFromRequest().get();
-    }
-
-    static User getUserInfoByToken() {
-        String token = request().getHeader("SITNET_TOKEN_HEADER_KEY");
-        Session session = (Session) Cache.get(SITNET_CACHE_KEY + token);
-        return Ebean.find(User.class, session.getUserId());
     }
 }
