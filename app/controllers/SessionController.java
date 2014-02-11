4a06b96ce49b6b954b0a2abb4b851b95c214767f
Miikka Liukkonen <miikka.liukkonen@arcusys.fi>
2014-02-11 06:45:26 +0200

    remove extra session checking

--- app/controllers/SessionController.java
+++ app/controllers/SessionController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import Exceptions.MalformedDataException;
+import Exceptions.UnauthorizedAccessException;
 import actions.Authenticate;
 
 import com.avaje.ebean.Ebean;
@@ -21,32 +22,23 @@ import java.util.UUID;
 
 public class SessionController extends SitnetController {
 
-    public static Result login() throws MalformedDataException {
+    public static Result login() throws MalformedDataException, UnauthorizedAccessException {
         Credentials credentials = bindForm(Credentials.class);
         Logger.debug("User login with username: {} and password: ***", credentials.getUsername());
         User user = Ebean.find(User.class).where().eq("email", credentials.getUsername()).eq("password", credentials.getPassword()).findUnique();
-        
-        // TODO: pitäisikö tässä heittää exception?
-        if(user == null)
-        {
-        	Logger.debug("UNAUTHORIZED!");
-        	return unauthorized("Incorrect username or password");
+        if (user == null) {
+            return unauthorized("Incorrect username or password.");
         }
-        
         String token = UUID.randomUUID().toString();
         Session session = new Session();
         session.setSince(DateTime.now());
         session.setUserId(user.getId());
         Cache.set(SITNET_CACHE_KEY + token, session);
-        
-		ObjectNode result = Json.newObject();
-
-		result.put("token", token);
-		result.put("firstname", user.getFirstName());
-		result.put("lastname", user.getLastName());
-		return ok(result);
-        
-//        return ok(token);
+        ObjectNode result = Json.newObject();
+        result.put("token", token);
+        result.put("firstname", user.getFirstName());
+        result.put("lastname", user.getLastName());
+        return ok(result);
     }
 
     @Authenticate
@@ -60,17 +52,8 @@ public class SessionController extends SitnetController {
     public static Result ping() {
         String token = request().getHeader(SITNET_TOKEN_HEADER_KEY);
         Session session = (Session) Cache.get(SITNET_CACHE_KEY + token);
-        
-        // TODO: session tarkistus
-        if(session == null)
-        {
-        	Logger.debug("UNAUTHORIZED!");
-        	return unauthorized("token not found");
-        }
-
         session.setSince(DateTime.now());
         Cache.set(SITNET_CACHE_KEY + token, session);
         return ok("pong");
-
     }
 }
