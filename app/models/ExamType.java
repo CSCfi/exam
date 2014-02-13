860e89558418fddc734d70005d8d1ec9850f47dd
Andrei Vainik <andrei.vainik@arcusys.fi>
2014-02-13 16:26:08 +0200

    Add new model

---
+++ app/models/ExamType.java
@@ -0,0 +1,20 @@
+package models;
+
+
+/*
+ * Opetuksen toteutus
+ * http://tietomalli.csc.fi/Opetuksen%20toteutus-kaavio.html
+ * 
+ * <Opetustapahtuman tyyppi> voi olla esim luento tai tentti.
+ * 
+ */
+public class ExamType {
+
+	// TDOD: miten erilaiset tenttityypit määritellään?
+	
+	// kaikille avoin, vain kutsusta, jne
+	private String type;
+	
+	
+	
+}
