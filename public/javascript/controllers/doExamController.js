8ececfea8f2e0f40eca5fc6adca4f58ee7983455
Mika Pellikka <mika.pellikka@arcusys.fi>
2014-03-04 16:21:06 +0200

    doexam implementation

--- public/javascript/controllers/doExamController.js
+++ public/javascript/controllers/doExamController.js
@@ -4,11 +4,7 @@
         .controller('DoExamController', ['$scope', '$routeParams', '$translate', '$http', 'SITNET_CONF', 'StudentExamRes', 'QuestionRes', 'dateService',
             function ($scope, $routeParams, $translate, $http, SITNET_CONF, StudentExamRes, QuestionRes, dateService) {
 
-
-
             $scope.sectionsBar = SITNET_CONF.TEMPLATES_PATH + "/student_sections_bar.html";
-
-
 
         	var param1 = $routeParams.param1;
         	console.log(param1);
@@ -17,6 +13,7 @@
                     $http.get('/student/doexam/'+$routeParams.hash)
                       .success(function(data, status, headers, config){
                         $scope.doexam = data;
+                        $scope.activeSection = $scope.doexam.examSections[0];
                       }).
                       error(function(data, status, headers, config) {
                           // called asynchronously if an error occurs
