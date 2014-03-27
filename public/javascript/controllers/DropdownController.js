19bf8f52aa09110cf8fc96c6d92ba0d93f30efd4
Mika Pellikka <mika.pellikka@arcusys.fi>
2014-03-27 08:49:02 +0200

    Dropdown menu with language selection and logout functionality implemented

--- public/javascript/controllers/DropdownController.js
+++ public/javascript/controllers/DropdownController.js
@@ -1,10 +1,23 @@
 (function () {
     'use strict';
     angular.module("sitnet.controllers")
-        .controller('DropdownCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+        .controller('DropdownCtrl', ['$scope', '$http', '$location', '$translate', 'SITNET_CONF', function ($scope, $http, $location, $translate, SITNET_CONF) {
+
+            // Todo: Miksei toimi?
+            var language = $translate("sitnet_user_language_change");
+            var logout = $translate("sitnet_logout");
+
+            $scope.handleClick = function(location, event) {
+//                var xhr = $http.post('/logout');
+                if (event) {
+                    event();
+                }
+                $location.path(location);
+            };
+
             $scope.items = [
-                { name: "Kieliasetukset", link: "#/user" },
-                { name: "Kirjaudu ulos", link: "#/logout" }
+                { name: "Kirjaudu ulos", link: "/logout", action: function() {console.log("aaaa")} }
             ];
+
         }]);
 }());
