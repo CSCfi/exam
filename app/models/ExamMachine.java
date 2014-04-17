d9af1b93173ce0e724a7fe7eff7a2e5fee9625b8
Mika Pellikka <mika.pellikka@arcusys.fi>
2014-04-17 14:42:18 +0300

    Added missing fields for exam machine adding form

--- app/models/ExamMachine.java
+++ app/models/ExamMachine.java
@@ -25,13 +25,27 @@ public class ExamMachine extends Model {
      */
     private String name;
 
+    /*
+     * Other identifier for the exam machine, specified in task SIT-84
+     */
+    private String otherIdentifier;
+
     // Esteettömyystieto
     private String accessibilityInfo;
+
+    // Checkbox indicating is there any accessibility issues concerning the room
+    private boolean accessible;
 
     // Ohjelmistot
     private String softwareInfo;
 
     private String ipAddress;
+
+    private String surveillanceCamera;
+
+    private String videoRecordings;
+
+    private boolean working;
 
     @ManyToOne
     @JsonBackReference
@@ -93,6 +107,14 @@ public class ExamMachine extends Model {
         this.accessibilityInfo = accessibilityInfo;
     }
 
+    public boolean isAccessible() {
+        return accessible;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.accessible = accessible;
+    }
+
     public String getSoftwareInfo() {
         return softwareInfo;
     }
@@ -127,5 +149,37 @@ public class ExamMachine extends Model {
 
     public void setReservation(Reservation reservation) {
         this.reservation = reservation;
+    }
+
+    public String getOtherIdentifier() {
+        return otherIdentifier;
+    }
+
+    public void setOtherIdentifier(String otherIdentifier) {
+        this.otherIdentifier = otherIdentifier;
+    }
+
+    public String getSurveillanceCamera() {
+        return surveillanceCamera;
+    }
+
+    public void setSurveillanceCamera(String surveillanceCamera) {
+        this.surveillanceCamera = surveillanceCamera;
+    }
+
+    public String getVideoRecordings() {
+        return videoRecordings;
+    }
+
+    public void setVideoRecordings(String videoRecordings) {
+        this.videoRecordings = videoRecordings;
+    }
+
+    public boolean isWorking() {
+        return working;
+    }
+
+    public void setWorking(boolean working) {
+        this.working = working;
     }
 }
