6a60063e4f250314c3daadb6471229d60fe1ef3e
Ari-Matti Lahtinen <ari-matti.lahtinen@arcusys.fi>
2014-05-26 15:11:56 +0300

    Modified mail sending logic

--- app/util/java/EmailComposer.java
+++ app/util/java/EmailComposer.java
@@ -1,9 +1,6 @@
 package util.java;
 
-import models.Exam;
-import models.ExamEnrolment;
-import models.ExamInspection;
-import models.User;
+import models.*;
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.FetchConfig;
 import com.avaje.ebean.Query;
@@ -16,6 +13,13 @@ import java.util.List;
 public class EmailComposer {
 
 
+    /**
+     *
+     * @param inspector The new inspector for the exam.
+     * @param sender    The teacher who assigned the inspector.
+     * @param exam      The exam to be inspected.
+     * @param msg       Message from the feedback form.
+     */
 
     public static void composeChangeInspectorNotification(User inspector, User sender, Exam exam, String msg) {
 
@@ -26,7 +30,6 @@ public class EmailComposer {
 
         String oql = "find examInspection " +
                      "fetch exam where (exam.name=:examId and exam.state=:estate) ";
-        //"where (state=:published or state=:saved or state=:draft) ";
 
         Query<ExamInspection> query = Ebean.createQuery(ExamInspection.class, oql);
 
@@ -70,5 +73,64 @@ public class EmailComposer {
         content += "</div></body></html>";
 
         EmailSender.sendInspectorNotification(recipient, sndr, subject, content);
+    }
+
+    /**
+     *
+     * @param inspector The responsible teacher for the exam.
+     * @param sender    The teacher who inspected the exam.
+     * @param exam      The exam.
+     * @param msg       Message from the feedback form.
+     */
+    public static void composeInspectionReadyNotification(User inspector, User sender, List<User> students, Exam exam, String msg) {
+
+        /*
+        <arvioinnin tehneen opettajan etunimi sukunimi>on tehnyt oman osansa arvioinnista. Arviointi koskee tenttiä <tentin nimi> <opintojaksokoodi>.
+
+        <saateviesti palautekentästä>
+
+        <linkki kyseisen tentin arviointinäkymään>
+         */
+
+        String examData = exam.getName() + "(" + exam.getCourse().getCode() + ")";
+        String subject = examData + " -tentti on annettu arvioitavaksesi";
+        String recipient = inspector.getFirstName() + " " + inspector.getLastName() + " <" + inspector.getEmail() + ">";
+        String sndr = sender.getFirstName() + " " + sender.getLastName() + " <" + sender.getEmail() + ">";
+
+        int notInspectedCount = students.size();
+
+        String content = "<html><body><div>" +
+                sender.getFirstName() + " " + sender.getLastName() +
+                " on merkinnyt sinut arvioijaksi seuraavalle tentille: " +
+                examData + "<br>" +
+                "Tentillä on " + notInspectedCount + " vastausta. <br><br>";
+
+        if (notInspectedCount > 0 && notInspectedCount < 6) {
+
+            content += "<ul>";
+
+            for (User usr : students) {
+
+                content += "<li>" + usr.getFirstName() + " " + usr.getLastName() + "</li>";
+
+            }
+            content += "</ul>";
+        }
+        /*
+        Jos edellinen lukumäärä on suurempi kuin nolla generoidaan:
+
+        • vastanneen opiskelijan/opiskelijoiden nimet <opiskelija> (listaus jos opiskelijoita esim. 1-5, muuten pelkkä lkm?)
+        */
+
+        content += "<div>" + msg + "</div><br><br>";
+
+        final String domain = "localhost:9000";
+
+        String linkToExam = "<a href=\"" + domain +"/#/home/\"" + ">Tenttinäkymään</a>";
+        content += linkToExam;
+        content += "</div></body></html>";
+
+        EmailSender.sendInspectorNotification(recipient, sndr, subject, content);
+
     }
 }
