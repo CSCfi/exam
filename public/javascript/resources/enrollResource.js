8a8af7ccb52bd78b631a2e8bd12ba2c39bc57876
Mika Pellikka <mika.pellikka@arcusys.fi>
2014-06-12 09:03:07 +0300

    enroll exam bug fix

--- public/javascript/resources/enrollResource.js
+++ public/javascript/resources/enrollResource.js
@@ -3,24 +3,24 @@
     angular.module("sitnet.resources")
         .factory("EnrollRes", ['$resource', function ($resource) {
             return {
-                    list: $resource("/enroll/:code",
-                    {
-                        code: "@code"
-                    },
-                    {
-                        "get":    {method: "GET", isArray: true, params: {code: "@code"}},
+                list: $resource("/enroll/:code",
+                {
+                    code: "@code"
+                },
+                {
+                    "get":    {method: "GET", isArray: true, params: {code: "@code"}},
 
-                    }),
-                    enroll: $resource("/enroll/:code/exam/:id",
-            		{
-                    	code: "@code", id: "@id"
-            		},
-            		{
-            			"get":    {method: "GET", isArray: false, params: {code: "@code", id: "@id"}},
-            			"create": {method: "POST", params: {code: "@code", id: "@id"}},
-            			
-            		})
+                }),
 
+                enroll: $resource("/enroll/:code/exam/:id",
+                {
+                    code: "@code", id: "@id"
+                },
+                {
+                    "get":    {method: "GET", isArray: false, params: {code: "@code", id: "@id"}},
+                    "create": {method: "POST", params: {code: "@code", id: "@id"}},
+
+                })
             }
         }]);
 }());
