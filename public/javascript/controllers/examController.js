17006d160fa91ec1f6b1b2e4280eadaea48da6c5
Andrei Vainik <andrei.vainik@arcusys.fi>
2014-07-29 13:04:05 +0300

    Fix SIT-372, Kysymysten arpominen, täytettävä kenttä ei näy. Set input min to 0.

--- public/javascript/controllers/examController.js
+++ public/javascript/controllers/examController.js
@@ -558,13 +558,13 @@
                 $scope.updateLotteryCount = function (section) {
 
                     if (section.lotteryItemCount > section.questions.length) {
-                        toastr.info("Arvonnassa ei voi olla enemmän kysymyksiä kuin osioon on lisättynä.");
+                        toastr.warning("Arvonnassa ei voi olla enemmän kysymyksiä kuin osioon on lisättynä.");
                         section.lotteryItemCount = section.questions.length;
                     }
 
                     else if (section.lotteryItemCount < 1) {
-                        toastr.info("Arvonnassa täytyy olla vähintään yksi kysymys.");
-                        section.lotteryItemCount = section.questions.length;
+                        toastr.warning("Arvonnassa täytyy olla vähintään yksi kysymys.");
+                        section.lotteryItemCount = 1;
                     }
 
                     else {
