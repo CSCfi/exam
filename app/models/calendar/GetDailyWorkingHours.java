55d5742c55509f745fbd01743aacb1909d0fe018
Miikka Liukkonen <miikka.liukkonen@arcusys.fi>
2014-07-30 15:40:27 +0300

    add missing changes

--- app/models/calendar/GetDailyWorkingHours.java
+++ app/models/calendar/GetDailyWorkingHours.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface GetDailyWorkingHours {
 
-    List<GetDailyWorkingHours> getDateTimeForDay(LocalDate)
+    List<GetDailyWorkingHours> getDateTimeForDay(LocalDate date);
 
-    :
+
 }
