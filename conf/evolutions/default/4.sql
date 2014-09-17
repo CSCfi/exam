07edf033d0b993dbef66dc865b0ee50add3cbef0
Andrei Vainik <andrei.vainik@arcusys.fi>
2014-09-17 22:58:45 +0300

    Fix 4.sql

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -1,3 +1,10 @@
+# --- !Ups
+
 alter table reservation add user_id bigint;
 alter table reservation add constraint fk_reservation_user foreign key (user_id) references sitnet_users (id) on delete restrict on update restrict;
 create index fk_reservation_user on reservation (user_id);
+
+# --- !Downs
+
+ALTER TABLE reservation DROP user_id;
+
