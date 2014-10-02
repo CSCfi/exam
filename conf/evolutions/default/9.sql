fab986237ef6316d463a7dd09c0367b8719f3616
Andrei Vainik <andrei.vainik@arcusys.fi>
2014-10-02 09:07:40 +0300

    Fix typo in 9.sql

--- conf/evolutions/default/9.sql
+++ conf/evolutions/default/9.sql
@@ -1,7 +1,7 @@
 # --- !Ups
 
 ALTER TABLE answer ADD attachment_id bigint;
-ALTER TABLE answer ADD CONSTRAINT fk_answer_attachment FOREIGN KEY (attachment_id) REFERENCES sitnet_attachment (id) on delete restrict on update restrict;
+ALTER TABLE answer ADD CONSTRAINT fk_answer_attachment FOREIGN KEY (attachment_id) REFERENCES attachment (id) on delete restrict on update restrict;
 CREATE index fk_answer_attachment ON answer (attachment_id);
 
 # --- !Downs
