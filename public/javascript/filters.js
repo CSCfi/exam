cfc899af4a0f219c8ef708e91f8111dc29748d53
Matti Lupari <matti.lupari@arcusys.fi>
2014-11-19 11:34:40 +0000

    SIT-714 display answer contents word count and character count

--- public/javascript/filters.js
+++ public/javascript/filters.js
@@ -15,5 +15,18 @@
                     date.getUTCSeconds());
             };
 
+        })
+        .filter('striphtml', function() {
+            return function(html) {
+                var div = document.createElement("div");
+                div.innerHTML = html;
+                var text = div.textContent || div.innerText || "";
+                return text.replace(/(^"|"$|\\n)/g,"").replace(/\s+/g, ' ').trim();
+            };
+        })
+        .filter('wordcount', function() {
+            return function(text) {
+                return text.split(" ").length;
+            };
         });
 }());
