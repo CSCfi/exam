def13e27954337f34b9831440bfb03f564a59e28
Matti Lupari <matti.lupari@arcusys.fi>
2014-12-15 14:23:12 +0000

    SIT-785 remake of mulitchoice question review layout

--- app/controllers/ExamController.java
+++ app/controllers/ExamController.java
@@ -338,7 +338,7 @@ public class ExamController extends SitnetController {
             options.setPathProperties("examSections.questions.answer.option", "id, option, correctOption, score");
             options.setPathProperties("examSections.questions.attachment", "id, fileName");
             options.setPathProperties("examSections.questions.answer.attachment", "id, fileName");
-            options.setPathProperties("examSections.questions.options", "id, option");
+            options.setPathProperties("examSections.questions.options", "id, option, correctOption");
             options.setPathProperties("examSections.questions.comments", "id, comment");
             options.setPathProperties("examFeedback", "id, comment");
 
