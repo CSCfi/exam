6a41c1694d53f3b9ef60fa8a3c9c077da3c82c94
Matti Lupari <matti.lupari@arcusys.fi>
2015-01-05 12:49:09 +0000

    SIT-685 model changes (initial)

---
+++ app/models/Language.java
@@ -0,0 +1,46 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class Language extends Model {
+
+    @Version
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Timestamp ebeanTimestamp;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+}
