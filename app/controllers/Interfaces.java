ecf7f8555c687b4fbd1c26151a69fa1780b97d14
Matti Lupari <matti.lupari@arcusys.fi>
2015-02-23 23:04:56 +0200

    SIT-953 search date must be in ISO-8601 format

--- app/controllers/Interfaces.java
+++ app/controllers/Interfaces.java
@@ -9,71 +9,26 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.typesafe.config.ConfigException;
 import com.typesafe.config.ConfigFactory;
 import exceptions.NotFoundException;
-import models.*;
+import models.Course;
+import models.ExamRecord;
+import models.Organisation;
 import models.dto.ExamScore;
-import play.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
 import play.libs.F;
 import play.libs.Json;
 import play.libs.WS;
 import play.mvc.Result;
 import play.mvc.Results;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class Interfaces extends SitnetController {
 
-    final static SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-
     private static final ObjectMapper SORTED_MAPPER = new ObjectMapper();
-
     static {
         SORTED_MAPPER.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
-    }
-
-    public static F.Promise<Result> getInfo(String code) throws NotFoundException {
-
-        String url = ConfigFactory.load().getString("sitnet.integration.courseUnitInfo.url");
-        if (url == null || url.isEmpty()) {
-            final List<Course> courses = Ebean.find(Course.class)
-                    .where()
-                    .eq("code", code)
-                    .findList();
-
-            return F.Promise.promise(new F.Function0<Result>() {
-                public Result apply() {
-                    return Results.ok(Json.toJson(courses));
-                }
-            });
-        }
-
-        try {
-            WS.WSRequestHolder ws = WS.url(url)
-                    .setTimeout(10 * 1000)
-                    .setQueryParameter("courseUnitCode", code);
-
-            return ws.get().map(
-                    new F.Function<WS.Response, Result>() {
-                        public Result apply(WS.Response response) {
-                            JsonNode json = response.asJson();
-                            return ok(json);
-                        }
-                    }
-            );
-        } catch (Exception ex) {
-            throw new NotFoundException(ex.getMessage());
-        }
-    }
-
-    private static boolean isSearchActive() {
-        try {
-            return ConfigFactory.load().getBoolean("sitnet.integration.courseUnitInfo.active");
-        } catch (ConfigException e) {
-            return false;
-        }
     }
 
     public static List<Course> getCourseInfo(String code) throws NotFoundException {
@@ -143,6 +98,19 @@ public class Interfaces extends SitnetController {
         return results;
     }
 
+    public static Result getNewRecords(String startDate) {
+        return ok(Json.toJson(getScores(startDate)));
+    }
+
+    // for testing purposes
+    public static Result getNewRecordsAlphabeticKeyOrder(String startDate) {
+        try {
+            return ok(convertNode(Json.toJson(getScores(startDate))));
+        } catch (JsonProcessingException e) {
+            return Results.internalServerError(e.getMessage());
+        }
+    }
+
     private static String getFirstChildNameValue(JsonNode json, String columnName) {
         if (json.has(columnName)) {
             JsonNode array = json.get(columnName);
@@ -156,155 +124,70 @@ public class Interfaces extends SitnetController {
         return null;
     }
 
-    public static Result getNewRecords(String startDate) {
-
-        Date start = null;
-
-        try {
-            start = sdf.parse(startDate);
-        } catch (ParseException e) {
-            return Results.badRequest("date format should be dd.MM.YYYY eg. 17.01.2011");
-        }
-
+    private static List<ExamScore> getScores(String startDate) {
+        DateTime start = ISODateTimeFormat.dateTimeParser().parseDateTime(startDate);
         List<ExamRecord> examRecords = Ebean.find(ExamRecord.class)
                 .select("exam_score")
                 .where()
-                .gt("time_stamp", start)
+                .gt("time_stamp", start.toDate())
                 .findList();
 
-        if (examRecords == null) {
-            return Results.ok("no records since: " + startDate);
-        }
-
-
-        List<ExamScore> examScores = new ArrayList<ExamScore>();
+        List<ExamScore> examScores = new ArrayList<>();
         for (ExamRecord record : examRecords) {
             examScores.add(record.getExamScore());
         }
-        return ok(Json.toJson(examScores));
+        return examScores;
     }
 
-    // for testing purposes
-    public static Result getNewRecordsAlphabeticKeyOrder(String startDate) {
-
-        Date start = null;
-
-        try {
-            start = sdf.parse(startDate);
-        } catch (ParseException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } finally {
-            if (start == null) {
-                return Results.badRequest("date format should be dd.MM.YYYY eg. 17.01.2011");
-            }
-        }
-
-        List<ExamRecord> examRecords = Ebean.find(ExamRecord.class)
-                .select("exam_score")
-                .where()
-                .gt("time_stamp", start)
-                .findList();
-
-        if (examRecords == null) {
-            return Results.ok("no records since: " + startDate);
-        }
-
-
-        List<ExamScore> examScores = new ArrayList<ExamScore>();
-        for (ExamRecord record : examRecords) {
-            examScores.add(record.getExamScore());
-        }
-
-        String json = null;
-        try {
-            json = convertNode(Json.toJson(examScores));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        return ok(json);
+    private static String convertNode(JsonNode node) throws JsonProcessingException {
+        Object obj = SORTED_MAPPER.treeToValue(node, Object.class);
+        return SORTED_MAPPER.writeValueAsString(obj);
     }
 
-    // for testing purposes
-    private static String convertNode(final JsonNode node) throws JsonProcessingException {
-        final Object obj = SORTED_MAPPER.treeToValue(node, Object.class);
-        final String json = SORTED_MAPPER.writeValueAsString(obj);
-        return json;
+    private static boolean isSearchActive() {
+        try {
+            return ConfigFactory.load().getBoolean("sitnet.integration.courseUnitInfo.active");
+        } catch (ConfigException e) {
+            return false;
+        }
     }
 
-    public static Result getRecords(String vatIdNumber, String startDate) {
+    // TODO: Is this useful for anything?
+    public static F.Promise<Result> getInfo(String code) throws NotFoundException {
 
-        //todo: ip limit
-        final Organisation organisation = Ebean.find(Organisation.class).where().eq("vatIdNumber", vatIdNumber).findUnique();
+        String url = ConfigFactory.load().getString("sitnet.integration.courseUnitInfo.url");
+        if (url == null || url.isEmpty()) {
+            final List<Course> courses = Ebean.find(Course.class)
+                    .where()
+                    .eq("code", code)
+                    .findList();
 
-        if (organisation == null) {
-            return notFound("no such organisation");
-        }
-
-        Date start = null;
-
-        try {
-            start = sdf.parse(startDate);
-        } catch (ParseException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } finally {
-            if (start == null) {
-                return Results.badRequest("date format should be dd.MM.YYYY eg. 17.01.2011");
-            }
-        }
-
-        final List<User> students = Ebean.find(User.class).where().eq("organisation", organisation).eq("roles.name", "STUDENT").findList();
-
-        List<ExamScore> examScores = new ArrayList<ExamScore>();
-
-        for (User student : students) {
-            for (ExamParticipation participation : student.getParticipations()) {
-
-                final Exam exam = participation.getExam();
-                Logger.info(exam.toString());
-                if (exam.getState().equals("GRADED")) {
-
-                    /*
-                    //todo:
-                    if(exam.getAnswerderDate().isBefore(startDate)) {
-                        continue;
-                    }
-                    */
-
-                    ExamScore examScore = new ExamScore();
-                    examScore.setIdentifier(student.getIdentifier());
-                    examScore.setCourseUnitCode(exam.getCourse().getCode());
-                    //todo: see this!
-                    examScore.setExamDate(sdf.format(exam.getModified()));
-                    examScore.setCredits(exam.getCourse().getCredits().toString());
-                    examScore.setCreditLanguage(exam.getAnswerLanguage());
-                    examScore.setStudentGrade(exam.getGrade());
-                    examScore.setGradeScale(exam.getGrading());
-/*
-                    List<String> scores = examScore.getExamScore();
-                    if(scores == null)
-                        scores = new ArrayList<String>();
-
-                    scores.add(exam.getTotalScore().toString());
-
-                    examScore.setExamScore(scores);
-
-*/
-                    examScore.setCourseUnitLevel(exam.getCourse().getLevel());
-//                    examScore.setCourseUnitType(exam.getCourse().getType().getName());
-                    //todo: fix this!
-                    examScore.setCreditType("");
-                    examScore.setLecturer("");
-                    examScore.setLecturerId("");
-                    examScore.setDate(sdf.format(new Date()));
-                    //todo: implementation
-                    examScore.setCourseImplementation("");
-                    examScores.add(examScore);
+            return F.Promise.promise(new F.Function0<Result>() {
+                public Result apply() {
+                    return Results.ok(Json.toJson(courses));
                 }
-
-            }
-
+            });
         }
-        return ok(Json.toJson(examScores));
+
+        try {
+            WS.WSRequestHolder ws = WS.url(url)
+                    .setTimeout(10 * 1000)
+                    .setQueryParameter("courseUnitCode", code);
+
+            return ws.get().map(
+                    new F.Function<WS.Response, Result>() {
+                        public Result apply(WS.Response response) {
+                            JsonNode json = response.asJson();
+                            return ok(json);
+                        }
+                    }
+            );
+        } catch (Exception ex) {
+            throw new NotFoundException(ex.getMessage());
+        }
     }
+
+
+
+
 }
