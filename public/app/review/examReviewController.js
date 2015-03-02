e97cc4061398782ee9baa6233f29709c3c6f24cd
Mikko Katajamäki <mikko.katajamaki@arcusys.fi>
2015-03-02 13:24:37 +0200

    fix

--- public/app/review/examReviewController.js
+++ public/app/review/examReviewController.js
@@ -436,7 +436,6 @@
                             if (!withoutNotice) {
                                 toastr.info($translate("sitnet_comment_updated"));
                             }
-                            $scope.examToBeReviewed.examFeedback.comment = comment;
                             $scope.startReview();
                         }, function (error) {
                             toastr.error(error.data);
