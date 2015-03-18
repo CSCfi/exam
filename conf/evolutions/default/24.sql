11812b71f545920e40ce78d53379575446d9a377
Mikko Katajamäki <mikko.katajamaki@arcusys.fi>
2015-03-18 13:53:30 +0200

    sql fix

--- conf/evolutions/default/24.sql
+++ conf/evolutions/default/24.sql
@@ -1,16 +1,12 @@
 # --- !Ups
-CREATE TABLE exam_owners (
+CREATE TABLE exam_owner (
   exam_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
-  CONSTRAINT pk_exam_owners PRIMARY KEY(exam_id, user_id)
+  CONSTRAINT pk_exam_owner PRIMARY KEY(exam_id, user_id)
 );
 
-ALTER TABLE exam ADD CONSTRAINT fk_exam_exam_owners_01 FOREIGN KEY(exam_id) REFERENCES exam_owners(exam_id);
-ALTER TABLE sitnet_users ADD CONSTRAINT fk_sitnet_users_exam_owners_01 FOREIGN KEY (user_id) REFERENCES exam_owners(user_id);
-
+ALTER TABLE exam_owner ADD CONSTRAINT fk_exam_owner_exam FOREIGN KEY(exam_id) REFERENCES exam(id);
+ALTER TABLE exam_owner ADD CONSTRAINT fk_exam_owner_sitnet_users FOREIGN KEY (user_id) REFERENCES sitnet_users(id);
 
 # --- !Downs
-ALTER TABLE exam DROP CONSTRAINT fk_exam_exam_owners_01;
-ALTER TABLE sitnet_users DROP CONSTRAINT fk_sitnet_users_exam_owners_01;
-
-DROP TABLE exam_owners;
+DROP TABLE exam_owner;
