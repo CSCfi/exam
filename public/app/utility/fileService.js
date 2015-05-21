8d473eee759f2a45ae30e68a4bcba685bf15b702
Matti Lupari <matti.lupari@arcusys.fi>
2015-05-21 13:41:28 +0300

    SIT-1095 attachment download in Safari made to work

--- public/app/utility/fileService.js
+++ public/app/utility/fileService.js
@@ -3,7 +3,25 @@
     angular.module('exam.services')
         .factory('fileService', ['$http', function ($http) {
 
-                var saveFile = function(data, filename) {
+            var supportsBlobUrls;
+
+            var svg = new Blob(
+                ["<svg xmlns='http://www.w3.org/2000/svg'></svg>"],
+                {type: "image/svg+xml;charset=utf-8"}
+            );
+            var img = new Image();
+            img.onload = function () {
+                supportsBlobUrls = true
+            };
+            img.onerror = function () {
+                supportsBlobUrls = false
+            };
+            img.src = URL.createObjectURL(svg);
+
+            var saveFile = function (data, filename) {
+                if (!supportsBlobUrls) {
+                    window.open('data:application/octet-stream;base64,' + data);
+                } else {
                     var byteString = atob(data);
                     var ab = new ArrayBuffer(byteString.length);
                     var ia = new Uint8Array(ab);
@@ -12,19 +30,20 @@
                     }
                     var blob = new Blob([ia], {type: "application/octet-stream"});
                     saveAs(blob, filename);
-                };
+                }
+            };
 
-                var download = function(url, filename, params) {
-                    $http.get(url, {params: params}).
-                        success(function (data) {
-                            saveFile(data, filename);
-                        }).
-                        error(function (error) {
-                            toastr.error(error.data || error);
-                        });
-                };
+            var download = function (url, filename, params) {
+                $http.get(url, {params: params}).
+                    success(function (data) {
+                        saveFile(data, filename);
+                    }).
+                    error(function (error) {
+                        toastr.error(error.data || error);
+                    });
+            };
 
-            return { download: download };
+            return {download: download};
         }]);
 }());
 
