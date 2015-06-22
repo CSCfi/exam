e3bdeea896ccd504d88798f6d1ee8449d98ccc16
Matti Lupari <matti.lupari@arcusys.fi>
2015-06-22 10:54:22 +0300

    make room for intermittent evolution

--- conf/evolutions/default/43.sql
+++ conf/evolutions/default/43.sql
@@ -1,8 +1,10 @@
 # --- !Ups
-ALTER TABLE question DROP COLUMN question_type;
-ALTER TABLE question ALTER type SET NOT NULL;
-ALTER TABLE answer DROP COLUMN answer_type;
-ALTER TABLE answer ALTER type SET NOT NULL;
+DROP SEQUENCE IF EXISTS course_unit_info_seq;
+DROP SEQUENCE IF EXISTS evaluation_criteria_seq;
+DROP SEQUENCE IF EXISTS evaluation_phrase_seq;
+DROP SEQUENCE IF EXISTS haka_attribute_seq;
+DROP SEQUENCE IF EXISTS material_seq;
+DROP SEQUENCE IF EXISTS mime_type_seq;
 
 # --- !Downs
 -- No going back
