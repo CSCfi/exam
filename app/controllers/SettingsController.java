c8c1fcc083d6a5d9c73b311aa725a45aa6f92121
Matti Lupari <matti.lupari@arcusys.fi>
2015-08-13 12:18:09 +0300

    SIT-000 fix updating user agreement

--- app/controllers/SettingsController.java
+++ app/controllers/SettingsController.java
@@ -35,7 +35,7 @@ public class SettingsController  extends BaseController {
         gs.update();
 
         // Since the EULA has changed, force users to accept it again.
-        String updStatement = "update sitnet_users set has_accepted_user_agreament = :hasNot";
+        String updStatement = "update app_user set has_accepted_user_agreament = :hasNot";
         Update<User> update = Ebean.createUpdate(User.class, updStatement);
         update.set("hasNot", false);
         update.execute();
