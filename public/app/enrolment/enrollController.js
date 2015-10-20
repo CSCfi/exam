7342c71fa91b48ed873bda25f3004d97c56fa4c7
Matti Lupari <matti.lupari@arcusys.fi>
2015-10-20 10:10:18 +0300

    SIT-000 enhancements for detailed enrolment info view

--- public/app/enrolment/enrollController.js
+++ public/app/enrolment/enrollController.js
@@ -6,25 +6,21 @@
 
                 $scope.enrollPath = EXAM_CONF.TEMPLATES_PATH + "enrolment/enroll.html";
                 $scope.examPath = EXAM_CONF.TEMPLATES_PATH + "enrolment/exam.html";
-                $scope.generalInfoPath = EXAM_CONF.TEMPLATES_PATH + "enrolment/detailed_info.html";
+                $scope.detailedInfoPath = EXAM_CONF.TEMPLATES_PATH + "enrolment/detailed_info.html";
 
-                if ($routeParams.code === undefined) {
-                    console.log($routeParams.code);
-
-                }
-                else if ($routeParams.code && $routeParams.id) {
+                if ($routeParams.code && $routeParams.id) {
 
                     EnrollRes.enroll.get({code: $routeParams.code, id: $routeParams.id},
                         function (exam) {
                             exam.languages = exam.examLanguages.map(function (lang) {
                                 return getLanguageNativeName(lang.code);
                             });
-                            $scope.exam = exam;
                             examService.setExamOwners(exam);
+                            $scope.exam = exam;
                             EnrollRes.check.get({id: exam.id}, function () {
-                                $scope.exam.alreadyEnrolled = true;
+                                $scope.exam.notEnrolled = false;
                             }, function () {
-                                $scope.exam.alreadyEnrolled = false;
+                                $scope.exam.notEnrolled = true;
                             });
                         },
                         function (error) {
