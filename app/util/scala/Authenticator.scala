cbc853fb44e6c16287bbf603ab1ac639565d1dd1
Matti Lupari <matti.lupari@arcusys.fi>
2015-11-10 15:41:30 +0200

    SIT-000 fix tests, EX-4 route user directly to SP-logout if no IdP-logout URL provided

--- app/util/scala/Authenticator.scala
+++ app/util/scala/Authenticator.scala
@@ -1,37 +1,46 @@
 package util.scala
 
 import com.avaje.ebean.Ebean
+import com.typesafe.config.ConfigFactory
 import models.{Session, User}
 import play.api.cache.CacheApi
 
 
 trait Authenticator {
 
-  val SITNET_TOKEN_HEADER_KEY = "x-exam-authentication"
+  val LOGIN_TYPE = ConfigFactory.load.getString("sitnet.login");
+
   val SITNET_CACHE_KEY = "user.session."
 
   val sessionCache: CacheApi
 
-  def getSession(token: String) = {
+  def getKey: String = LOGIN_TYPE match {
+    case "HAKA" => "Shib-Session-Id"
+    case _ => "x-exam-authentication"
+  }
+
+  def getSession(token: String): Session = {
     sessionCache.getOrElse[Session](SITNET_CACHE_KEY + token) {
       null
     }
   }
 
-  def getLoggedUser(token: String) = {
-    Ebean.find(classOf[User], getSession(token).getUserId)
+  def getLoggedUser(token: String): User = getSession(token) match {
+    case session: Any =>
+      Ebean.find(classOf[User], getSession(token).getUserId)
+    case _ => null
   }
 
-  def getAuthorizedUser(token: String, roles: Seq[String]) = {
-    getLoggedUser(token) match {
-      case user: Any =>
-        val loginRole = getSession(token).getLoginRole
-        roles.contains(loginRole) match {
+  def getAuthorizedUser(token: String, roles: Seq[String]): User = getLoggedUser(token) match {
+    case user: Any =>
+      getSession(token) match {
+        case session: Any => roles.contains(session.getLoginRole) match {
           case true => user
           case _ => null
         }
-      case _ => null
-    }
+        case _ => null
+      }
+    case _ => null
   }
 
 }
