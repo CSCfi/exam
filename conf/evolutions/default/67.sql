c4de6d863d791f7f3be3a949154f2af0f8d84de7
Matti Lupari <matti.lupari@arcusys.fi>
2015-12-29 14:06:40 +0200

    SIT-000 add roles and exam types via evolution script

--- conf/evolutions/default/67.sql
+++ conf/evolutions/default/67.sql
@@ -1,8 +1,21 @@
 # --- !Ups
-ALTER TABLE exam DROP grade;
-ALTER TABLE exam DROP exam_grade_id;
+INSERT INTO role (id, name, object_version) SELECT 1, 'ADMIN', 1 WHERE NOT EXISTS (
+    SELECT id FROM role WHERE id = 1
+);
+INSERT INTO role (id, name, object_version) SELECT 2, 'STUDENT', 1 WHERE NOT EXISTS (
+    SELECT id FROM role WHERE id = 2
+);
+INSERT INTO role (id, name, object_version) SELECT 3, 'TEACHER', 1 WHERE NOT EXISTS (
+    SELECT id FROM role WHERE id = 3
+);
+
+INSERT INTO exam_type (id, type, deprecated, object_version) SELECT 1, 'PARTIAL', false, 1 WHERE NOT EXISTS (
+    SELECT id FROM exam_type WHERE id = 1
+);
+INSERT INTO exam_type (id, type, deprecated, object_version) SELECT 2, 'FINAL', false, 1 WHERE NOT EXISTS (
+    SELECT id FROM exam_type WHERE id = 2
+);
 
 # --- !Downs
-ALTER TABLE exam ADD grade VARCHAR(255);
-ALTER TABLE exam ADD exam_grade_id BIGINT;
+-- N/A
 
