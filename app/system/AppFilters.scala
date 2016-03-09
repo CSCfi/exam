5b1b1d19aeabc67c994c06069857a08e4e22a0eb
Valtteri Rouvinen <valtteri.rouvinen@arcusys.fi>
2016-03-09 13:37:26 +0200

    SIT-1442 Fixed performance problem in reservation view.
    - Added gzip compression to responses.
    - Problem was in select menu initalisation with ui-select2.

--- app/system/AppFilters.scala
+++ app/system/AppFilters.scala
@@ -4,8 +4,9 @@ import javax.inject.Inject
 
 import play.api.http.HttpFilters
 import play.filters.csrf.CSRFFilter
+import play.filters.gzip.GzipFilter
 import play.filters.headers.SecurityHeadersFilter
 
-class AppFilters @Inject() (securityHeadersFilter: SecurityHeadersFilter, csrfFilter: CSRFFilter) extends HttpFilters {
-  override def filters = Seq(securityHeadersFilter, csrfFilter)
+class AppFilters @Inject() (securityHeadersFilter: SecurityHeadersFilter, csrfFilter: CSRFFilter, gzipFilter: GzipFilter) extends HttpFilters {
+  override def filters = Seq(securityHeadersFilter, csrfFilter, gzipFilter)
 }
