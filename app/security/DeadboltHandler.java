0b9e0b5b02f228cc28d4a62d903295c9facef32f
Matti Lupari <matti.lupari@arcusys.fi>
2016-03-10 11:56:15 +0200

    SIT-000 use direct ehcache access in deadbolt handler

--- app/security/DeadboltHandler.java
+++ app/security/DeadboltHandler.java
@@ -6,7 +6,9 @@ import com.avaje.ebean.Ebean;
 import controllers.BaseController;
 import models.Session;
 import models.User;
-import play.cache.Cache;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import play.mvc.Http;
 import play.mvc.Result;
 
@@ -15,7 +17,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
 
+
 public class DeadboltHandler extends AbstractDeadboltHandler {
+
+    // Can't use DI here
+    private Cache cache = CacheManager.getInstance().getCache("play");
 
     @Override
     public CompletableFuture<Optional<Result>> beforeAuthCheck(Http.Context context) {
@@ -25,20 +31,26 @@ public class DeadboltHandler extends AbstractDeadboltHandler {
     @Override
     public CompletionStage<Optional<? extends Subject>> getSubject(final Http.Context context) {
         String token = BaseController.getToken(context);
-        Session session = (Session) Cache.get(BaseController.SITNET_CACHE_KEY + token);
-        User user = session == null ? null : Ebean.find(User.class, session.getUserId());
-        // filter out roles not found in session
-        if (user != null) {
-            user.setRoles(user.getRoles().stream()
-                    .filter((r) -> r.getName().equals(session.getLoginRole()))
-                    .collect(Collectors.toList()));
+        Element element = cache.get(BaseController.SITNET_CACHE_KEY + token);
+        if (element != null) {
+            Session session = (Session) element.getObjectValue();
+            User user = session == null ? null : Ebean.find(User.class, session.getUserId());
+            // filter out roles not found in session
+            if (user != null) {
+                user.setRoles(user.getRoles().stream()
+                        .filter((r) -> r.getName().equals(session.getLoginRole()))
+                        .collect(Collectors.toList()));
+            }
+            return CompletableFuture.supplyAsync(() -> Optional.ofNullable(user));
+        } else {
+            return CompletableFuture.supplyAsync(Optional::empty);
         }
-        return CompletableFuture.supplyAsync(() -> Optional.ofNullable(user));
     }
 
     @Override
     public CompletableFuture<Result> onAuthFailure(Http.Context context, String content) {
         return  CompletableFuture.supplyAsync(() -> forbidden("Authentication failure"));
     }
+
 
 }
