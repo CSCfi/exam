f55640bf031c4dff7577d009acca91da5c705e48
Matti Lupari <matti.lupari@arcusys.fi>
2016-05-11 10:42:21 +0300

    SIT-1517 fix cloning of multiple choice options

--- app/models/questions/MultipleChoiceOption.java
+++ app/models/questions/MultipleChoiceOption.java
@@ -73,7 +73,7 @@ public class MultipleChoiceOption extends GeneratedIdentityModel implements Comp
 
     public MultipleChoiceOption copy() {
         MultipleChoiceOption option = new MultipleChoiceOption();
-        BeanUtils.copyProperties(this, option, "id, examSectionQuestionOptions");
+        BeanUtils.copyProperties(this, option, "id", "examSectionQuestionOptions");
         return option;
     }
 
