6d302254bbbad169ff9fdb3d9c6796ac5dfbe7d8
Valtteri Rouvinen <valtteri.rouvinen@arcusys.fi>
2016-06-30 13:13:17 +0300

    SIT-1580 Disabled angular animations to faster testing

--- protractor/conf.js
+++ protractor/conf.js
@@ -6,5 +6,14 @@ exports.config = {
     onPrepare: function () {
         global.EC = protractor.ExpectedConditions;
         browser.driver.manage().window().setSize(1200, 1000);
+
+        // Disable animations so e2e tests run more quickly
+        var disableNgAnimate = function() {
+            angular.module('disableNgAnimate', []).run(['$animate', function($animate) {
+                $animate.enabled(false);
+            }]);
+        };
+
+        browser.addMockModule('disableNgAnimate', disableNgAnimate);
     }
 };
