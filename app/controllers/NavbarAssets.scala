3a72c5c79a0f07eeda38e879b7a84d690b0b9298
Matti Lupari <matti.lupari@arcusys.fi>
2016-09-19 15:52:12 +0300

    SIT-1568 cleanup

--- app/controllers/NavbarAssets.scala
+++ app/controllers/NavbarAssets.scala
@@ -8,7 +8,7 @@ class NavbarAssets extends Controller {
   def at(path: String, file: String) = EssentialAction { request =>
     val Pattern = "(.*%7B%7Blink.icon_png%7D%7D)".r
     file match {
-      case Pattern(foo) => controllers.Assets.at(path, "assets/images/1x1.png")(request)
+      case Pattern(x) => controllers.Assets.at(path, "assets/images/1x1.png")(request)
       case _ => controllers.Assets.at(path, file)(request)
     }
   }
