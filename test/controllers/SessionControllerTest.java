b77e5ed6b32f49e16bced73a01e2ac6dda0ed7f5
Matti Lupari <matti.lupari@arcusys.fi>
2017-03-27 12:08:49 +0300

    SIT-1807 identification of temporarily visiting student during login

--- test/controllers/SessionControllerTest.java
+++ test/controllers/SessionControllerTest.java
@@ -2,9 +2,12 @@ package controllers;
 
 import base.IntegrationTestCase;
 import com.avaje.ebean.Ebean;
+import models.Reservation;
 import models.Role;
 import models.User;
+import org.joda.time.DateTime;
 import org.junit.Test;
+import play.mvc.Result;
 
 import static org.fest.assertions.Assertions.assertThat;
 
@@ -25,6 +28,35 @@ public class SessionControllerTest extends IntegrationTestCase {
         assertThat(user.getRoles().get(0).getName()).isEqualTo(Role.Name.TEACHER.toString());
         assertThat(user.getFirstName()).isEqualTo("George");
         assertThat(user.getLastName()).isEqualTo("Lazenby");
+
+    }
+
+    @Test
+    public void testLoginAsTemporalStudentVisitor() throws Exception {
+        String eppn = "newuser@test.org";
+        User user = Ebean.find(User.class).where().eq("eppn", eppn).findUnique();
+        assertThat(user).isNull();
+
+        Reservation reservation = new Reservation();
+        reservation.setExternalRef("abcd1234");
+        reservation.setExternalUserRef(eppn);
+        reservation.setStartAt(DateTime.now().plusHours(2).toDate());
+        reservation.setEndAt(DateTime.now().plusHours(3).toDate());
+        reservation.save();
+
+        login(eppn);
+
+        user = Ebean.find(User.class).where().eq("eppn", eppn).findUnique();
+        assertThat(user).isNotNull();
+        assertThat(user.getRoles()).hasSize(1);
+        assertThat(user.getRoles().get(0).getName()).isEqualTo(Role.Name.TEACHER.toString());
+
+        reservation = Ebean.find(Reservation.class).where().eq("externalRef", "abcd1234").findUnique();
+        assertThat(reservation.getUser().getId()).isEqualTo(user.getId());
+
+        // Try do some teacher stuff, see that it is not allowed
+        Result result = get("/app/reviewerexams");
+        assertThat(result.status()).isEqualTo(403);
 
     }
 }
