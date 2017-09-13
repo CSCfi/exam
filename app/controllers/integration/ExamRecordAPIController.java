02772bb193407a416c229067fe89275cfcbe2e6f
Matti Lupari <matti.lupari@arcusys.fi>
2017-09-13 15:45:35 +0300

    SIT-1871 Active Exams API

---
+++ app/controllers/integration/ExamRecordAPIController.java
@@ -0,0 +1,59 @@
+package controllers.integration;
+
+
+import be.objectify.deadbolt.java.actions.SubjectNotPresent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import controllers.base.BaseController;
+import io.ebean.Ebean;
+import models.ExamRecord;
+import models.dto.ExamScore;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+import play.libs.Json;
+import play.mvc.Result;
+import play.mvc.Results;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ExamRecordAPIController extends BaseController {
+
+    private static final ObjectMapper SORTED_MAPPER = new ObjectMapper();
+    static {
+        SORTED_MAPPER.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+    }
+
+    @SubjectNotPresent
+    public Result getNewRecords(String startDate) {
+        return ok(Json.toJson(getScores(startDate)));
+    }
+
+    // for testing purposes
+    @SubjectNotPresent
+    public Result getNewRecordsAlphabeticKeyOrder(String startDate) {
+        try {
+            return ok(convertNode(Json.toJson(getScores(startDate))));
+        } catch (JsonProcessingException e) {
+            return Results.internalServerError(e.getMessage());
+        }
+    }
+
+    private static List<ExamScore> getScores(String startDate) {
+        DateTime start = ISODateTimeFormat.dateTimeParser().parseDateTime(startDate);
+        List<ExamRecord> examRecords = Ebean.find(ExamRecord.class)
+                .fetch("examScore")
+                .where()
+                .gt("timeStamp", start.toDate())
+                .findList();
+        return examRecords.stream().map(ExamRecord::getExamScore).collect(Collectors.toList());
+    }
+
+    private static String convertNode(JsonNode node) throws JsonProcessingException {
+        Object obj = SORTED_MAPPER.treeToValue(node, Object.class);
+        return SORTED_MAPPER.writeValueAsString(obj);
+    }
+
+}
