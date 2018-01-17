91207e33089b43a42ab87bd1c502703a1d2031f6
Valtteri Rouvinen <valtteri.rouvinen@arcusys.fi>
2018-01-17 08:51:06 +0200

    SIT-0000 Fixed role and eula dialogs. (#15)

--- app/frontend/src/session/eula/eulaDialog.component.js
+++ app/frontend/src/session/eula/eulaDialog.component.js
@@ -16,30 +16,8 @@
 import angular from 'angular';
 
 angular.module('app.session')
-    .component('eula', {
-        template: '<div id="sitnet-dialog">\n' +
-        '\n' +
-        '    <div class="student-message-dialog-wrapper-padding">\n' +
-        '        <div class="student-enroll-dialog-wrap">\n' +
-        '            <div class="student-enroll-title">{{\'sitnet_accept_useragreement\' | translate}}</div>\n' +
-        '        </div>\n' +
-        '        <div class="modal-body">\n' +
-        '            <div ng-bind-html="$ctrl.settings.eula.value">\n' +
-        '            </div>\n' +
-        '        </div>\n' +
-        '        <div class="student-message-dialog-footer">\n' +
-        '            <div class="student-message-dialog-button-save">\n' +
-        '                <button class="btn btn-sm btn-primary" ng-click="$ctrl.ok()">\n' +
-        '                    {{\'sitnet_button_accept\' | translate}}\n' +
-        '                </button>\n' +
-        '            </div>\n' +
-        '            <div class="student-message-dialog-button-cancel">\n' +
-        '                <button class="btn btn-sm btn-danger pull-left" ng-click="$ctrl.cancel()">{{\'sitnet_button_decline\' | translate}}\n' +
-        '                </button>\n' +
-        '            </div>\n' +
-        '        </div>\n' +
-        '    </div>\n' +
-        '</div>\n',
+    .component('eulaDialog', {
+        template: require('./eulaDialog.template.html'),
         bindings: {
             close: '&',
             dismiss: '&'
