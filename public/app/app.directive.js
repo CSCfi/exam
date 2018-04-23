115c64af3d0f61193c4c50bcd85c65da02f57a14
Matti Lupari <matti.lupari@arcusys.fi>
2018-04-23 17:15:07 +0300

    CSCEXAM-84 more debouncing of ckeditor events

--- public/app/app.directive.js
+++ public/app/app.directive.js
@@ -210,9 +210,10 @@ angular.module('app')
                     });
                 }
 
-                ck.on('change', lodash.debounce(updateModel, 100)); // This can bring down the UI if not scaled down
-                ck.on('dataReady', updateModel);
-                ck.on('key', lodash.debounce(updateModel, 100));
+                // These events can bring down the UI if not debounced
+                ck.on('change', _.debounce(updateModel, 500));
+                ck.on('dataReady', _.debounce(updateModel, 500));
+                ck.on('key', _.debounce(updateModel, 500));
                 ck.on('mode', updateModel); // Editing mode change
 
                 ngModel.$render = function (value) {
