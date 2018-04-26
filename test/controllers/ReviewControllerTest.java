93b3aead6d0458a83f2c857894af51a5ab51b5d1
Valtteri Rouvinen <valtteri.rouvinen@arcusys.fi>
2018-04-26 09:20:34 +0300

    CSCEXAM-18 Added assessment tab to admin exam view.

--- test/controllers/ReviewControllerTest.java
+++ test/controllers/ReviewControllerTest.java
@@ -17,6 +17,7 @@
 package controllers;
 
 import base.IntegrationTestCase;
+import base.RunAsAdmin;
 import base.RunAsTeacher;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -37,6 +38,48 @@ public class ReviewControllerTest extends IntegrationTestCase {
     @RunAsTeacher
     public void getExamReviews() {
         final User user = Ebean.find(User.class, userId);
+        final Exam parent = setUpReviews(user);
+
+        // Execute
+        Result result = get("/app/reviews/" + parent.getId());
+
+        // Verify
+        assertThat(result.status()).isEqualTo(200);
+        JsonNode node = Json.parse(contentAsString(result));
+        ArrayNode participationArray = (ArrayNode) node;
+        assertThat(participationArray.size()).isEqualTo(1);
+
+        final JsonNode participation = participationArray.get(0);
+        final JsonNode examGradeScale = participation
+                .path("exam").path("gradeScale");
+        assertThat(examGradeScale).isNotEmpty();
+        final JsonNode examGrades = examGradeScale.path("grades");
+        assertThat(examGrades.size()).isEqualTo(2);
+
+        final JsonNode courseGradeScale = participation
+                .path("exam").path("course").path("gradeScale");
+        assertThat(courseGradeScale).isNotEmpty();
+        final JsonNode courseGrades = courseGradeScale.path("grades");
+        assertThat(courseGrades.size()).isEqualTo(6);
+    }
+
+    @Test
+    @RunAsAdmin
+    public void getExamReviewsAsAdmin() {
+        final User user = Ebean.find(User.class, 3L);
+        final Exam parent = setUpReviews(user);
+
+        // Execute
+        Result result = get("/app/reviews/" + parent.getId());
+
+        // Verify
+        assertThat(result.status()).isEqualTo(200);
+        JsonNode node = Json.parse(contentAsString(result));
+        ArrayNode participationArray = (ArrayNode) node;
+        assertThat(participationArray.size()).isEqualTo(1);
+    }
+
+    private Exam setUpReviews(User user) {
         final Exam parent = Ebean.find(Exam.class).where()
                 .and()
                 .eq("name", "Algoritmit, 2013")
@@ -50,27 +93,6 @@ public class ReviewControllerTest extends IntegrationTestCase {
         examInspection.save();
         parent.getExamInspections().add(examInspection);
         parent.save();
-
-        // Execute
-        Result result = get("/app/reviews/" + parent.getId());
-
-        // Verify
-        assertThat(result.status()).isEqualTo(200);
-        JsonNode node = Json.parse(contentAsString(result));
-        ArrayNode participations = (ArrayNode) node;
-        assertThat(participations.size()).isEqualTo(1);
-
-        final JsonNode participation = participations.get(0);
-        final JsonNode examGradeScale = participation
-                .path("exam").path("gradeScale");
-        assertThat(examGradeScale).isNotEmpty();
-        final JsonNode examGrades = examGradeScale.path("grades");
-        assertThat(examGrades.size()).isEqualTo(2);
-
-        final JsonNode courseGradeScale = participation
-                .path("exam").path("course").path("gradeScale");
-        assertThat(courseGradeScale).isNotEmpty();
-        final JsonNode courseGrades = courseGradeScale.path("grades");
-        assertThat(courseGrades.size()).isEqualTo(6);
+        return parent;
     }
 }
