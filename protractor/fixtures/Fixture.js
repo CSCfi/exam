a48949c06f9ac708c6e8de87747840996248b427
Matti Lupari <matti.lupari@arcusys.fi>
2019-09-24 16:12:09 +0300

    CSCEXAM-000 get rid of ancient sql-fixtures dependency

--- protractor/fixtures/Fixture.js
+++ protractor/fixtures/Fixture.js
@@ -1,32 +1,49 @@
 let path = require('path');
 let sqlFixtures = require('../../app/frontend/node_modules/sql-fixtures');
+const {Pool} = require('../../app/frontend/node_modules/pg');
+const _ = require('../../app/frontend/node_modules/lodash');
+const users = require('./users.json');
+const courses = require('./courses.json');
+const organisations = require('./organisations.json');
+const questions = require('./questions.json');
+const exams = require('./exams.json');
+
 let fs = require('fs');
 let mergeJSON = require('../../app/frontend/node_modules/merge-json');
 
-let Fixture = function () {
+let Fixture = async function () {
 
-    let baseData = ['users.json', 'courses.json', 'organisations.json', 'questions.json', 'exams.json'];
+    const baseData = [users, courses, organisations, questions, exams];
 
-    let dbConfig = {
-        client: 'pg',
-        connection: {
-            host: 'localhost',
-            user: 'sitnet',
-            password: 'sitnetsitnet',
-            database: 'sitnet_protractor',
-            port: 5432
-        }
+    const pool = new Pool({
+        user: 'sitnet',
+        host: 'localhost',
+        database: 'sitnet_protractor',
+        password: 'sitnetsitnet',
+        port: 5432,
+    });
+
+    const createQuery = (table, data) => {
+        const keys = Object.keys(data[0]);
+        const keyPart = keys.join(',');
+        return data.map(d => {
+            let enumeration = [...Array(keys.length).keys()].map(k => '$' + (k + 1));
+            let stmt = `INSERT INTO ${table} (${keyPart}) VALUES (${enumeration.join(', ')})`;
+            return {
+                text: stmt,
+                values: Object.values(d)
+            }
+        })
     };
 
     this.loadFixtures = function (files) {
         files = files || baseData;
         console.log("Loading fixtures from files: " + files);
-        let data = {};
-        for (let i=0; i < files.length; i++) {
-            let p = path.resolve(__dirname, files[i]);
-            data = mergeJSON.merge(data, JSON.parse(fs.readFileSync(p, 'utf8')));
-        }
-        return sqlFixtures.create(dbConfig, data);
+        const data = files.reduce((a, b) => _.merge(a, b), {});
+        const relations = Object.keys(data).map(k => ({'table': k, 'data': data[k]}));
+        relations.forEach(r => {
+            pool.query(createQuery(r.table, r.data));
+        });
     };
 
     this.clearFixtures = function () {
@@ -36,14 +53,12 @@ let Fixture = function () {
         for (let i = 0; i < tables.length; i++) {
             queries.push('truncate table ' + tables[i] + ' cascade');
         }
-        return sqlFixtures.create(dbConfig, {
-            sql: queries
-        })
+        queries.forEach(q => pool.query(q));
     };
 
-    this.destroy = function () {
+    this.destroy = async function () {
         console.log("Destroy fixtures...");
-        return sqlFixtures.destroy();
+        await client.end();
     }
 };
 module.exports = Fixture;
