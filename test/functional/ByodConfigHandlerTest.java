f21148e6367e315e5cc2057321fb04973ef2b915
Matti Lupari <matti.lupari@arcusys.fi>
2020-01-07 10:30:35 +0200

    CSCEXAM-000 add test for checking SEB config key calculation

---
+++ test/functional/ByodConfigHandlerTest.java
@@ -0,0 +1,32 @@
+package functional;
+
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+
+import backend.util.config.ByodConfigHandler;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.failure;
+import static play.test.Helpers.running;
+
+public class ByodConfigHandlerTest {
+
+    private Application app = new GuiceApplicationBuilder().build();
+
+    @Test
+    public void testCalculateConfigKey() {
+        running(app, () -> {
+            ByodConfigHandler bch = app.injector().instanceOf(ByodConfigHandler.class);
+            try {
+                String key = bch.calculateConfigKey("123456");
+                assertThat(key).isEqualTo("485445b7b7df8fe16b2b7e736eb74f6aaf203924d0ea92d87e179bb5a4c0be43");
+            } catch (Exception e) {
+                throw failure("exception thrown inside handler");
+            }
+        });
+
+    }
+
+}
+
