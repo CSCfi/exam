47948fed00e55dcca5301a4396bd2c58e8581b11
Matti Lupari <matti.lupari@arcusys.fi>
2020-02-26 13:24:05 +0200

    CSCEXAM-117 disallow external reservations for exams with optional sections - Once again :)

--- app/backend/controllers/iop/transfer/impl/ExternalCalendarController.java
+++ app/backend/controllers/iop/transfer/impl/ExternalCalendarController.java
@@ -72,6 +72,7 @@ import backend.models.MailAddress;
 import backend.models.Reservation;
 import backend.models.User;
 import backend.models.iop.ExternalReservation;
+import backend.models.sections.ExamSection;
 import backend.sanitizers.Attrs;
 import backend.sanitizers.ExternalCalendarReservationSanitizer;
 import backend.security.Authenticated;
@@ -297,6 +298,9 @@ public class ExternalCalendarController extends CalendarController {
         Optional<Result> error = checkEnrolment(enrolment, user, sectionIds);
         if (error.isPresent()) {
             return wrapAsPromise(error.get());
+        }
+        if (enrolment.getExam().getExamSections().stream().anyMatch(ExamSection::isOptional)) {
+            return wrapAsPromise(forbidden("Optional sections not supported for external reservations"));
         }
         // Lets do this
         URL url = parseUrl(orgRef, roomRef);
