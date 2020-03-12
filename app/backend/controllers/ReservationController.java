06e3e772664255d66e32050035d92f1b61de747f
Matti Lupari <matti.lupari@arcusys.fi>
2020-03-12 13:50:20 +0200

    CSCEXAM-490 remove no-show reservation if private exam in question (#389)

--- app/backend/controllers/ReservationController.java
+++ app/backend/controllers/ReservationController.java
@@ -143,26 +143,12 @@ public class ReservationController extends BaseController {
 
     @Restrict({@Group("TEACHER"), @Group("ADMIN")})
     public Result permitRetrial(Long id) {
-        Reservation reservation = Ebean.find(Reservation.class)
-                .fetch("enrolment.exam.executionType")
-                .where()
-                .idEq(id)
-                .findOne();
+        Reservation reservation = Ebean.find(Reservation.class, id);
         if (reservation == null) {
             return notFound("sitnet_not_found");
         }
-        ExamEnrolment enrolment = reservation.getEnrolment();
-        if (reservation.isNoShow() && enrolment.getExam().isPrivate()) {
-            // For no-shows with private examinations we remove the reservation so student can re-reserve.
-            // This is needed because student is not able to re-enroll by himself.
-            enrolment.setReservation(null);
-            enrolment.update();
-            reservation.delete();
-        } else {
-            // For public exams this is not necessary as student can re-enroll and make a new reservation in the process.
-            reservation.setRetrialPermitted(true);
-            reservation.update();
-        }
+        reservation.setRetrialPermitted(true);
+        reservation.update();
         return ok();
     }
 
