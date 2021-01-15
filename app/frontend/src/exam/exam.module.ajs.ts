bac2e554540958e21b09fc788c8fb41b38598f96
Matti Lupari <matti.lupari@gmail.com>
2021-01-15 20:27:12 +0200

    Http interceptor & exan summary migration

--- app/frontend/src/exam/exam.module.ajs.ts
+++
@@ -1,7 +0,0 @@
-import { downgradeComponent } from '@angular/upgrade/static';
-
-import { ExamTabsComponent } from './editor/examTabs.component';
-
-export default angular
-    .module('app.exam.editor', [])
-    .directive('examTabs', downgradeComponent({ component: ExamTabsComponent })).name;
