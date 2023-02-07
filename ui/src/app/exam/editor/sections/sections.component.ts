17c8af32e47b184457e9db51e8552ce093ce9f0c
Matti Lupari <matti.lupari@gmail.com>
2023-02-07 16:52:39 +0200

    CSCEXAM-1068 Fix updating of question indices after moval (#884)
    Co-authored-by: Matti Lupari <matti.lupari@arcusys.fi>

--- ui/src/app/exam/editor/sections/sections.component.ts
+++ ui/src/app/exam/editor/sections/sections.component.ts
@@ -67,6 +67,7 @@ export class SectionsComponent implements OnInit, OnChanges {
             this.Exam.reorderSections$(from, to, this.exam, this.collaborative).subscribe({
                 next: () => {
                     moveItemInArray(this.exam.examSections, from, to);
+                    this.updateIndices();
                     this.toast.info(this.translate.instant('sitnet_sections_reordered'));
                 },
                 error: this.toast.error,
@@ -130,6 +131,8 @@ export class SectionsComponent implements OnInit, OnChanges {
         }
         return this.exam.executionType.type === 'PUBLIC';
     };
+
+    private updateIndices = () => this.exam.examSections.forEach((es, i) => (es.sequenceNumber = i));
 
     private init = () => {
         this.exam.examSections.sort((a, b) => a.sequenceNumber - b.sequenceNumber);
