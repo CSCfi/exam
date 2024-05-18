f6fcf5ca569ab8c115ca76b324b6ba2a82576a2a
Matti Lupari <matti.lupari@gmail.com>
2024-05-18 23:05:20 +0300

    Serve MathJax from node_modules (by copying to assets)

---
+++ app/controllers/assets/ScriptController.scala
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2024 The members of the EXAM Consortium
+// SPDX-FileCopyrightText: 2024. The members of the EXAM Consortium
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+package controllers.assets
+
+import controllers.Assets
+import play.api.mvc._
+
+import javax.inject._
+
+@Singleton
+class ScriptController @Inject() (assets: Assets, cc: ControllerComponents) extends AbstractController(cc):
+
+  def index: Action[AnyContent] = assets.at("index.html")
+
+  def assetOrDefault(resource: String): Action[AnyContent] =
+    //if resource.contains("assets/scripts") then assets.at("../../node_modules/mathjax/MathJax.js")
+    if resource.contains(".") then assets.at(resource) else index
