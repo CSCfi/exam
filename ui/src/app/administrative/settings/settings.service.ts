0c8fab3119586b0f372559f527f46b7bd45746b0
Matti Lupari <matti.lupari@gmail.com>
2025-01-28 21:13:38 +0200

    CSCEXAM-1404 User agreement prompt for all user roles

--- ui/src/app/administrative/settings/settings.service.ts
+++ ui/src/app/administrative/settings/settings.service.ts
@@ -10,14 +10,15 @@ import { AppConfig } from 'src/app/administrative/administrative.model';
 export class SettingsService {
     constructor(private http: HttpClient) {}
 
-    updateAgreement$ = (config: AppConfig) => this.http.put('/app/settings/agreement', { value: config.eula });
+    updateAgreement$ = (config: AppConfig, bypassAgreementUpdate = false) =>
+        this.http.put('/app/settings/agreement', { value: config.eula, majorUpdate: bypassAgreementUpdate });
 
     updateDeadline$ = (config: AppConfig) => this.http.put('/app/settings/deadline', { value: config.reviewDeadline });
 
     updateReservationWindow$ = (config: AppConfig) =>
         this.http.put('/app/settings/reservationWindow', { value: config.reservationWindowSize });
 
-    listAttributes$ = () => this.http.get<string[]>('/attributes');
+    listAttributes$ = () => this.http.get<string[]>('/app/attributes');
 
     getConfig$ = () => this.http.get<AppConfig>('/app/config');
 }
