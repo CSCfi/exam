2252f1a5063e910a54b6be93c8a0295056839b9b
Matti Lupari <matti.lupari@gmail.com>
2025-02-27 08:15:10 +0200

    CSCEXAM-1422 Don't enforce builtin display in SEB config

--- test/functional/ByodConfigHandlerTest.java
+++ test/functional/ByodConfigHandlerTest.java
@@ -11,27 +11,21 @@ public class ByodConfigHandlerTest extends IntegrationTestCase {
 
     @Test
     public void testCalculateConfigKey() {
-        running(
-            app,
-            () -> {
-                ByodConfigHandler bch = app.injector().instanceOf(ByodConfigHandler.class);
-                String key = bch.calculateConfigKey("123456", "quit");
-                assertThat(key).isEqualTo("8bc05b365a8093e0aeb7f4c3c3021a077bf454f12d1e232dcdef2b6f3d1098f7");
-            }
-        );
+        running(app, () -> {
+            ByodConfigHandler bch = app.injector().instanceOf(ByodConfigHandler.class);
+            String key = bch.calculateConfigKey("123456", "quit");
+            assertThat(key).isEqualTo("f775f524e898feaab8f110ac76ee49ea0b1c5a62b5ce53d24b53fce1132d2168");
+        });
     }
 
     @Test
     public void testCreateConfigFile() {
-        running(
-            app,
-            () -> {
-                ByodConfigHandler bch = app.injector().instanceOf(ByodConfigHandler.class);
-                byte[] pwd = bch.getEncryptedPassword("password", "salt");
-                byte[] data = bch.getExamConfig("123456", pwd, "salt", "quit");
-                // sanity check that we actually have a reasonably sized file content
-                assertThat(data.length).isGreaterThan(1000);
-            }
-        );
+        running(app, () -> {
+            ByodConfigHandler bch = app.injector().instanceOf(ByodConfigHandler.class);
+            byte[] pwd = bch.getEncryptedPassword("password", "salt");
+            byte[] data = bch.getExamConfig("123456", pwd, "salt", "quit");
+            // sanity check that we actually have a reasonably sized file content
+            assertThat(data.length).isGreaterThan(1000);
+        });
     }
 }
