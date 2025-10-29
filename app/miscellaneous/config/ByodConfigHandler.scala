5ee229887cd02eec7e1059f72e918be5642f317f
Matti Lupari <matti.lupari@gmail.com>
2025-10-29 14:58:19 +0200

    CSCEXAM-1431 Support SEB's new JavaScript API

--- app/miscellaneous/config/ByodConfigHandler.scala
+++ app/miscellaneous/config/ByodConfigHandler.scala
@@ -4,13 +4,11 @@
 
 package miscellaneous.config
 
-import play.mvc.{Http, Result}
-
-import java.util.Optional
+import play.api.mvc.{RequestHeader, Result}
 
 trait ByodConfigHandler:
   def getExamConfig(hash: String, pwd: Array[Byte], salt: String, quitPwd: String): Array[Byte]
   def calculateConfigKey(hash: String, quitPwd: String): String
   def getPlaintextPassword(pwd: Array[Byte], salt: String): String
   def getEncryptedPassword(pwd: String, salt: String): Array[Byte]
-  def checkUserAgent(request: Http.RequestHeader, examConfigKey: String): Optional[Result]
+  def checkUserAgent(request: RequestHeader, examConfigKey: String): Option[Result]
