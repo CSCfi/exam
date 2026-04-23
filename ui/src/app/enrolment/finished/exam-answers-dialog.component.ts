eef35590b3e4d6971fff97ffdc23359934442e1e
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2026-04-23 12:18:00 +0300

    Escape HTML in multi-choice answers

--- ui/src/app/enrolment/finished/exam-answers-dialog.component.ts
+++ ui/src/app/enrolment/finished/exam-answers-dialog.component.ts
@@ -20,6 +20,14 @@ import { CourseCodeComponent } from 'src/app/shared/miscellaneous/course-code.co
     imports: [TranslateModule, MathJaxDirective, UpperCasePipe, DatePipe, CourseCodeComponent],
 })
 export class ExamAnswersDialogComponent implements OnInit {
+    private static readonly HTML_ESCAPE_MAP: Record<string, string> = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+    };
+
     @Input() exam!: Exam;
     @Input() participationTime = '';
     @Input() participationDuration: number | string = 0;
@@ -40,6 +48,8 @@ export class ExamAnswersDialogComponent implements OnInit {
     countWords = (answer: ExamSectionQuestion) => this.CommonExam.countWords(answer.essayAnswer?.answer);
     countCharacters = (answer: ExamSectionQuestion) => this.CommonExam.countCharacters(answer.essayAnswer?.answer);
     getAnsweredOptions = (answer: ExamSectionQuestion) => answer.options.filter((o) => o.answered);
+    escapeHtml = (value?: string) =>
+        (value ?? '').replace(/[&<>"']/g, (ch) => ExamAnswersDialogComponent.HTML_ESCAPE_MAP[ch] ?? ch);
     isMultiChoice = (answer: ExamSectionQuestion) =>
         ['WeightedMultipleChoiceQuestion', 'MultipleChoiceQuestion', 'ClaimChoiceQuestion'].indexOf(
             answer.question.type,
