508b1954122f88f6d127603b9786ab1f9ba24ac4
Matti Lupari <matti.lupari@csc.fi>
2026-09-21 10:24:31 +0300

    CSCEXAM-1638 Fixed clearing a grade

--- test/assessment/ReviewControllerSpec.scala
+++ test/assessment/ReviewControllerSpec.scala
@@ -9,7 +9,7 @@ import base.BaseIntegrationSpec
 import database.EbeanQueryExtensions
 import io.ebean.DB
 import models.assessment.ExamInspection
-import models.exam.Exam
+import models.exam.*
 import models.user.User
 import play.api.http.Status
 import play.api.libs.json.*
@@ -31,6 +31,25 @@ class ReviewControllerSpec extends BaseIntegrationSpec with EbeanQueryExtensions
 
   private def examParentId: Long =
     exam.get.parent.id // Safe since we validate exam exists in setup
+
+  private def gradeNamed(name: String): Grade =
+    DB.find(classOf[Grade]).where().eq("name", name).eq(
+      "gradeScale.id",
+      exam.get.gradeScale.id
+    ).find.getOrElse(fail(s"No grade $name found"))
+
+  private def reload(): Exam = DB.find(classOf[Exam], exam.get.id)
+
+  private def reviewBody(state: String, grade: Option[Int], gradingType: String): JsObject =
+    Json.obj(
+      "id"             -> JsNumber(BigDecimal(exam.get.id)),
+      "state"          -> state,
+      "gradingType"    -> gradingType,
+      "creditType"     -> "FINAL",
+      "answerLanguage" -> "de",
+      "customCredit"   -> 5.0,
+      "additionalInfo" -> JsNull
+    ) ++ grade.fold(Json.obj())(g => Json.obj("grade" -> JsNumber(BigDecimal(g))))
 
   // Custom login methods that automatically set up exam inspection
   private def loginAsTeacherWithExamInspection(): (User, Session) =
@@ -80,3 +99,59 @@ class ReviewControllerSpec extends BaseIntegrationSpec with EbeanQueryExtensions
         json.mustBe(a[JsArray])
         val participationArray = json.as[JsArray]
         participationArray.value must have size 1
+
+    "clearing a previously set grade" should:
+      "remove the grade and put the exam back to review" in:
+        val (_, session) = loginAsAdminWithExamInspection()
+        val grade        = gradeNamed("APPROVED")
+
+        val graded = runIO(
+          put(
+            s"/app/review/${exam.get.id}",
+            reviewBody("GRADED", Some(grade.id.intValue), "GRADED"),
+            session = session
+          )
+        )
+        statusOf(graded) must be(Status.OK)
+        reload().grade.id must be(grade.id)
+
+        // Picking the empty option in the grade dropdown sends no grade at all
+        val cleared = runIO(
+          put(
+            s"/app/review/${exam.get.id}",
+            reviewBody("REVIEW_STARTED", None, "GRADED"),
+            session = session
+          )
+        )
+        statusOf(cleared) must be(Status.OK)
+        val updated = reload()
+        Option(updated.grade) must be(None)
+        updated.gradingType must be(GradeType.GRADED)
+        updated.state must be(ExamState.REVIEW_STARTED)
+
+    "clearing the 'not graded' selection" should:
+      "reset the grading type back to graded" in:
+        val (_, session) = loginAsAdminWithExamInspection()
+
+        val notGraded = runIO(
+          put(
+            s"/app/review/${exam.get.id}",
+            reviewBody("GRADED", None, "NOT_GRADED"),
+            session = session
+          )
+        )
+        statusOf(notGraded) must be(Status.OK)
+        reload().gradingType must be(GradeType.NOT_GRADED)
+
+        val cleared = runIO(
+          put(
+            s"/app/review/${exam.get.id}",
+            reviewBody("REVIEW_STARTED", None, "GRADED"),
+            session = session
+          )
+        )
+        statusOf(cleared) must be(Status.OK)
+        val updated = reload()
+        Option(updated.grade) must be(None)
+        updated.gradingType must be(GradeType.GRADED)
+        updated.state must be(ExamState.REVIEW_STARTED)
